13260e739a49c9f9ccab49cec3b61189b0351a56
Gathurian <git@gathurian.xyz>
2024-10-19 02:30:57 +0200

    Add colorscheme and markview

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,6 @@
+-- I have no idea what it does, but LazyVim breaks if this line is not present
+vim.g.bigfile_size=1024*1024*1 -- 1M
+
 require("config.options")
 require("config.keybindings")
 require("config.lazy")
